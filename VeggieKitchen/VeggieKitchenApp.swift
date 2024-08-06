ff7ac0454e5c92d1dc363943ca5b7fb69f22725a
julie <julie.ryan.france@gmail.com>
2024-08-06 19:45:37 +0200

    Initial Commit

---
+++ VeggieKitchen/VeggieKitchenApp.swift
@@ -0,0 +1,32 @@
+//
+//  VeggieKitchenApp.swift
+//  VeggieKitchen
+//
+//  Created by julie ryan on 06/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VeggieKitchenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
